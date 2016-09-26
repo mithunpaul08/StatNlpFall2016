47576a064aec8ba33ce68174811712948d6784bf
mithun <mithunpaul08@gmail.com>
2016-09-25 20:31:22 -0700

    created frequency distribution for test data also

--- assignment2/qn4.py
+++ assignment2/qn4.py
@@ -10,25 +10,51 @@ from nltk.tokenize import sent_tokenize
 
 #define what is the input
 fname1="trainingDataInputForQn1.txt"
-fname2="BiGramOutputForQn3.txt"
-fname3="UniGramOutputForQn3.txt"
-fname4="TriGramOutputForQn3.txt"
+fname2= "testData.txt"
 lambda1=0
 lambda2=0
 lambda3=0
 
 
- #write every sentence in a new line into the file
+ #read contents of the training data file
 f= open(fname1)
 content = f.read()
 
-#tokenize the entire text, not just setences.
+#tokenize the entire text in training data, not just setences.
 tokens = nltk.word_tokenize(content)
-tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
-bi_tokens = bigrams(tokens)
-tri_tokens = trigrams(tokens)
+uni_tokens_training = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
+bi_tokens_training = bigrams(uni_tokens_training)
+tri_tokens_training = trigrams(uni_tokens_training)
 
-fdist1 = nltk.FreqDist(tri_tokens)
+fdist_train_tri = nltk.FreqDist(tri_tokens_training)
+fdist__train_bi = nltk.FreqDist(bi_tokens_training)
+fdist_train_uni= nltk.FreqDist(uni_tokens_training)
+#print the most common 50 bigrams, but with the frequency counts
+#print fdist2.most_common(50)
+
+
+
+#################end of training data manipulations#############
+
+ #read contents of the training data file
+f2 = open(fname2)
+content_test = f2.read()
+#tokenize the entire text in test data, not just setences.
+tokens_testing = nltk.word_tokenize(content_test)
+uni_tokens_testing = [token_testing.lower() for token_testing in tokens_testing if len(token_testing) > 1] #same as unigrams
+bi_tokens_testing = bigrams(tokens_testing)
+tri_tokens_testing = trigrams(tokens_testing)
+
+fdist_test_tri = nltk.FreqDist(tri_tokens_testing)
+fdist__test_bi = nltk.FreqDist(bi_tokens_testing)
+fdist_test_uni= nltk.FreqDist(uni_tokens_testing)
+#print the most common 50 bigrams, but with the frequency counts
+print fdist_test_tri.most_common(50)
+
+
+#################end of testing data manipulations#############
+
+
 # print the most common top 50 n-grams -but without the frequency value
 #vocabulary1 = fdist1.keys()
 #print vocabulary1[:50]
@@ -36,12 +62,10 @@ fdist1 = nltk.FreqDist(tri_tokens)
 #print fdist1.most_common(50)
 
 
-fdist2 = nltk.FreqDist(bi_tokens)
+
 # print the most common top 50 n-grams -but without the frequency value
 #vocabulary1 = fdist1.keys()
 #print vocabulary1[:50]
-#print the most common 50 words, but with the frequency counts
-print fdist2.most_common(50)
 
 
 #for k,v in fdist.items():
--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit 
+git commit -m "$1"
 git push
