2de6c1fffe4e8b651392532392a6a84cc6eaed5a
mithun <mithunpaul08@gmail.com>
2016-09-25 21:24:47 -0700

    found lambda for uni,bi and trigrams

--- assignment2/qn4.py
+++ assignment2/qn4.py
@@ -27,7 +27,7 @@ bi_tokens_training = bigrams(uni_tokens_training)
 tri_tokens_training = trigrams(uni_tokens_training)
 
 fdist_train_tri = nltk.FreqDist(tri_tokens_training)
-fdist__train_bi = nltk.FreqDist(bi_tokens_training)
+fdist_train_bi = nltk.FreqDist(bi_tokens_training)
 fdist_train_uni= nltk.FreqDist(uni_tokens_training)
 #print the most common 50 bigrams, but with the frequency counts
 #print fdist2.most_common(50)
@@ -46,11 +46,53 @@ bi_tokens_testing = bigrams(tokens_testing)
 tri_tokens_testing = trigrams(tokens_testing)
 
 fdist_test_tri = nltk.FreqDist(tri_tokens_testing)
-fdist__test_bi = nltk.FreqDist(bi_tokens_testing)
+fdist_test_bi = nltk.FreqDist(bi_tokens_testing)
 fdist_test_uni= nltk.FreqDist(uni_tokens_testing)
 #print the most common 50 bigrams, but with the frequency counts
-print fdist_test_tri.most_common(50)
+#print fdist_test_tri.most_common(50)
 
+#for k,v in fdist_test_tri_sorted():
+#    print k,v
+
+#m="('the', 'United', 'States')"
+# if m in fdist_test_tri.keys():
+#      print "unigram word found"
+# else:
+#     print "given ngram not found"
+
+#if the given ngram in trainign data exists in test data increaes the count of lambda
+for uniKeys in fdist_train_uni.keys():
+    if uniKeys in fdist_test_uni.keys():
+        print uniKeys
+        print "unigram word found"
+        lambda1=lambda1+1
+
+#if the given ngram in trainign data exists in test data increaes the count of lambda
+for biKeys in fdist_train_bi.keys():
+    if biKeys in fdist_test_bi.keys():
+        print biKeys
+        print "bigram word found"
+        lambda2=lambda2+1
+
+#if the given ngram in trainign data exists in test data increaes the count of lambda
+for triKeys in fdist_train_tri.keys():
+    if triKeys in fdist_test_tri.keys():
+        print triKeys
+        print "trigram word found"
+        lambda3=lambda3+1
+
+print "\n"
+print "\nLambda 1:" + lambda1
+print "\nLambda 2:" + lambda2
+print "\nLambda 3:" + lambda3
+                                                    #     author_bigram_dictionary[m]["count"] += 1
+                                                    #
+                                                    # else:
+                                                    #     author_bigram_dictionary[m] = {}
+                                                    #     author_bigram_dictionary[m]["count"] = 1
+#fdist_test_tri_sorted=sorted(fdist_test_tri.items())
+#for k,v in fdist_test_tri_sorted():
+#    print k,v
 
 #################end of testing data manipulations#############
 
